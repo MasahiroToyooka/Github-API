33cc1e0fd632ecece0d79fc3473392f4466f89d3
MasahiroToyooka <ehara2481@gmail.com>
2020-09-17 23:08:15 +0900

    Home画面作成

--- Github-API/Github-API/Home/HomeViewController.swift
+++ Github-API/Github-API/Home/HomeViewController.swift
@@ -14,16 +14,45 @@ protocol HomeViewInterface: class {
 }
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultTableView: UITableView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     private var presenter: HomePresenter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         presenter = HomePresenter(self)
+        initializeTableView()
     }
 
+    private func initializeTableView() {
+        resultTableView.delegate = self
+        resultTableView.dataSource = self
+        resultTableView.register(R.nib.searchResultCell)
+    }
 
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.searchResultCell, for: indexPath)!
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        searchTextField.resignFirstResponder()
+    }
 }
 
 extension HomeViewController: HomeViewInterface {
---
+++ Github-API/Github-API/Home/SearchResultCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResultCell.swift
+//  Github-API
+//
+//  Created by 豊岡正紘 on 2020/09/17.
+//  Copyright © 2020 Masahiro Toyooka. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
