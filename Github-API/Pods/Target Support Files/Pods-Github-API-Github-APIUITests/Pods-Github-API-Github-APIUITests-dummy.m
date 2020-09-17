9265d7584c64b08a63da3509cc3624ba6b269df7
MasahiroToyooka <ehara2481@gmail.com>
2020-09-17 22:46:51 +0900

    初期設定

---
+++ Github-API/Pods/Target Support Files/Pods-Github-API-Github-APIUITests/Pods-Github-API-Github-APIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Github_API_Github_APIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Github_API_Github_APIUITests
+@end
---
+++ Github-API/Pods/Target Support Files/Pods-Github-APITests/Pods-Github-APITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Github_APITests : NSObject
+@end
+@implementation PodsDummy_Pods_Github_APITests
+@end
